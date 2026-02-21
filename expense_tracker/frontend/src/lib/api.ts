f91068b43545b079183fef036b43ff5a849efa9f
Bashir Ahmed Chandio <chandiobashirahmed44@gmail.com>
2026-02-21 23:20:22 +0500

    Updated baseURL for API to the new endpoint.

--- expense_tracker/frontend/src/lib/api.ts
+++ expense_tracker/frontend/src/lib/api.ts
@@ -1,9 +1,1 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://full-stack-projects-tkpa.vercel.app/api",
-  withCredentials: true,
-});
-
-export default api;
-
+export const baseURL = 'https://spendwise-ten.vercel.app/api';
