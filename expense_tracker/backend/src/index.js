eb672a7ff80ec90683e28877b96dbcae66588a1c
Bashir Ahmed Chandio <chandiobashirahmed44@gmail.com>
2026-02-22 13:02:03 +0500

    Update index.js

--- expense_tracker/backend/src/index.js
+++ expense_tracker/backend/src/index.js
@@ -15,7 +15,10 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: (process.env.FRONTEND_URL || "http://localhost:5173").replace(
+      /\/$/,
+      "",
+    ),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
